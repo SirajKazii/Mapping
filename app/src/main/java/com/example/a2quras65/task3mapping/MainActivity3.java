b828a77e1eca29c42a8714a7c20c0508562049b1
2quras65 <2quras65>
2017-03-16 10:01:46 +0000

    Initial commit

---
+++ app/src/main/java/com/example/a2quras65/task3mapping/MainActivity3.java
@@ -0,0 +1,13 @@
+package com.example.a2quras65.task3mapping;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main3);
+    }
+}

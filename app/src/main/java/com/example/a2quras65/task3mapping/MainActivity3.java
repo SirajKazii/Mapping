99b0b6cce9172a3e6f01a1f076a33a3deb024987
SirajKazii <www.srj893@gmail.com>
2017-03-16 11:00:38 +0000

    Added basic map

--- app/src/main/java/com/example/a2quras65/task3mapping/MainActivity3.java
+++ app/src/main/java/com/example/a2quras65/task3mapping/MainActivity3.java
@@ -1,13 +1,32 @@
 package com.example.a2quras65.task3mapping;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import org.osmdroid.views.MapView;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.config.Configuration;
 
-public class MainActivity3 extends AppCompatActivity {
+public class MainActivity3 extends Activity
+{
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    MapView mv;
+
+    public void onCreate(Bundle savedInstanceState)
+    {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
+
+        // this line tells OpenStreetMap about our app.
+        // If you miss this out, you might get banned from OSM servers
+        Configuration.getInstance().load
+                (this, PreferenceManager.getDefaultSharedPreferences(this));
+
+        mv = (MapView)findViewById(R.id.map1);
+
+        mv.setBuiltInZoomControls(true);
+        mv.getController().setZoom(14);
+        mv.getController().setCenter(new GeoPoint(51.05,-0.72));
     }
 }
